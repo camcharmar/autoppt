f28a90c267f68dc7c15af557e603c69bb7c322d5
Cameron Mara <camcharmar@gmail.com>
2021-08-31 11:31:58 -0500

    useEffect to load ad

--- src/components/ads/Leaderboard1.js
+++ src/components/ads/Leaderboard1.js
@@ -1,13 +1,14 @@
-export const Leaderboard1 = () => (
-    <div id="345822648" dangerouslySetInnerHTML={{
-        __html: `
-        <script type="text/javascript">
-            try {
-                window._mNHandle.queue.push(function (){
-                    window._mNDetails.loadTag("345822648", "728x90", "345822648");
-                });
-            }
-            catch (error) {}
-        </script>`,
-    }}></div>
-);
+import { useEffect } from "react";
+
+export const Leaderboard1 = () => {
+    useEffect(() => {
+        try {
+            window._mNHandle.queue.push(function (){
+                window._mNDetails.loadTag("345822648", "728x90", "345822648");
+            });
+        }
+        catch (error) {}
+    }, []);
+
+    return <div id="345822648" />;
+};
